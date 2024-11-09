976db8e2431b5fd49f799d65dd75dcea629f4e8e
Vadimvr <r.vadim.90@gmail.com>
2024-11-09 22:31:09 +0300

    Добавьте файлы проекта.

---
+++ BloodyLightingXML/SledAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BloodyLightingXML
+{
+    public class SledAnimation
+    {
+        public string? Name { get; set; }
+        public Guid? Guid { get; set; }
+        public string Description { get; set; } = "ckAnimation:For custom-made each frame。";
+        public int Time { get; set; } = 0;
+        public string BackgroundColor = "000000";
+        [XmlIgnoreAttribute]
+        public List<Frame>? Frames { get; set; } = new List<Frame>();
+
+        public int FrameCount { get; set; }
+    }
+
+    public class Frame
+    {
+        public List<string> ColorPicture { get; set; } = (new string[18]).Select(x => x = "000000").ToList();
+        public double DisplayTime { get; set; } = 0.005;
+    }
+}
