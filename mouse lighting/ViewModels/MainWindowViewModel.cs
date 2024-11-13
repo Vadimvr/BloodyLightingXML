c5547959bcd5e7d447541f4660eef3707fb37a06
Vadimvr <r.vadim.90@gmail.com>
2024-11-13 17:13:22 +0300

    Added title

--- mouse lighting/ViewModels/MainWindowViewModel.cs
+++ mouse lighting/ViewModels/MainWindowViewModel.cs
@@ -13,7 +13,7 @@ namespace mouse_lighting.ViewModels
         #region Title : string - Заголовок окна
 
         /// <summary>Заголовок окна</summary>
-        private string _Title = "Главное окно";
+        private string _Title = "Lighting for the bloody mouse";
 
         /// <summary>Заголовок окна</summary>
         public string Title { get => _Title; set => Set(ref _Title, value); }
